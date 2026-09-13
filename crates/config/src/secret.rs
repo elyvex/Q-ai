143931ecc01bdbc7860a9f07b0d38ee11696e2ca
Elyvex <hjpd2546@outlook.com>
2026-09-13 05:58:47 +0330

    Modernize Secret: use ZeroizeOnDrop, support String, add into_inner

--- crates/config/src/secret.rs
+++ crates/config/src/secret.rs
@@ -1,13 +1,18 @@
 use std::fmt;
 use std::ops::Deref;
-use zeroize::{Zeroize, DefaultIsZeroes};
+use zeroize::{Zeroize, ZeroizeOnDrop};
 
 /// Wrapper that redacts secrets in Debug, Display, and Serialize.
-pub struct Secret<T: Zeroize + DefaultIsZeroes> {
+///
+/// The inner value is zeroized on drop (requires [`Zeroize`]).
+/// `String`, `Vec<u8>`, and zeroable primitives all implement
+/// `Zeroize`; `DefaultIsZeroes` is *not* required so `Secret<String>`
+/// works as designed in D0.5.
+pub struct Secret<T: Zeroize> {
     inner: T,
 }
 
-impl<T: Zeroize + DefaultIsZeroes> Secret<T> {
+impl<T: Zeroize> Secret<T> {
     pub fn new(inner: T) -> Self {
         Self { inner }
     }
@@ -15,52 +20,56 @@ impl<T: Zeroize + DefaultIsZeroes> Secret<T> {
     pub fn expose(&self) -> &T {
         &self.inner
     }
+
+    pub fn into_inner(self) -> T {
+        self.inner
+    }
 }
 
-impl<T: Zeroize + DefaultIsZeroes> Drop for Secret<T> {
+impl<T: Zeroize> Drop for Secret<T> {
     fn drop(&mut self) {
         self.inner.zeroize();
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes + Default> Default for Secret<T> {
+impl<T: Zeroize + Default> Default for Secret<T> {
     fn default() -> Self {
         Self::new(T::default())
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes> From<T> for Secret<T> {
+impl<T: Zeroize> From<T> for Secret<T> {
     fn from(inner: T) -> Self {
         Self::new(inner)
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes + Clone> Clone for Secret<T> {
+impl<T: Zeroize + Clone> Clone for Secret<T> {
     fn clone(&self) -> Self {
         Self::new(self.inner.clone())
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes> Deref for Secret<T> {
+impl<T: Zeroize> Deref for Secret<T> {
     type Target = T;
     fn deref(&self) -> &Self::Target {
         &self.inner
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes> fmt::Debug for Secret<T> {
+impl<T: Zeroize> fmt::Debug for Secret<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str("Secret(*** )")
+        f.write_str("Secret(***)")
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes> fmt::Display for Secret<T> {
+impl<T: Zeroize> fmt::Display for Secret<T> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.write_str("***")
     }
 }
 
-impl<T: Zeroize + DefaultIsZeroes + serde::Serialize> serde::Serialize for Secret<T> {
+impl<T: Zeroize + serde::Serialize> serde::Serialize for Secret<T> {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where
         S: serde::Serializer,
@@ -69,7 +78,7 @@ impl<T: Zeroize + DefaultIsZeroes + serde::Serialize> serde::Serialize for Secre
     }
 }
 
-impl<'de, T: Zeroize + DefaultIsZeroes + serde::Deserialize<'de>> serde::Deserialize<'de> for Secret<T> {
+impl<'de, T: Zeroize + serde::Deserialize<'de>> serde::Deserialize<'de> for Secret<T> {
     fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
     where
         D: serde::Deserializer<'de>,
@@ -77,3 +86,5 @@ impl<'de, T: Zeroize + DefaultIsZeroes + serde::Deserialize<'de>> serde::Deseria
         T::deserialize(deserializer).map(Secret::new)
     }
 }
+
+impl<T: Zeroize> ZeroizeOnDrop for Secret<T> {}
